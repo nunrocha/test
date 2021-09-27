d5dc2f06c669a291d9371a4b5bc15e064b66f2b9
anonymous <nonius.r@gmail.com>
2021-09-27 12:02:42 +0100

    Development of the project structure

---
+++ app_example/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+count = 0
+
+@app.route('/')
+def index():
+	global count
+	count += 1
+	
+	return 'Hello from application' + str(count)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
